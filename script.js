6612ef098dea1d825799e24042b57ab3e2487a4b
Scigni <ignaciolobaiza@gmail.com>
2022-06-22 23:34:21 -0300

    Added appear animations on content

--- script.js
+++ script.js
@@ -6,11 +6,6 @@ let content = new Array(document.querySelector("#about-me"),
                         document.querySelector("#skills"),
                         document.querySelector("#projects"));
 
-for (let i = 0; i < content.length; i++) {
-    content[i].style.setProperty("display", "none", "important");
-}
-
-
 btnAboutMe.addEventListener("click", function(){
     makeInvisibleCurrent(content);
     makeVisible(content[0]);
@@ -27,21 +22,20 @@ btnProjects.addEventListener("click", function(){
 });
 
 
-function fadeActiveElement(elem){
-    console.log(elem);
-}
-
-
 function makeVisible(elem) {
-    elem.style.display = "inherit!important";
-    elem.style.setProperty("display", "inherit", "important");
+    elem.style.setProperty("position", "relative", "important");
+    elem.style.visibility = "visible";
+    elem.className += " content-appear";
 }
 
 
 function makeInvisibleCurrent(listElem) {
     for (let i = 0; i < listElem.length; i++) {
-        if (listElem[i].style.display == "inherit") {
-            listElem[i].style.setProperty("display", "none", "important");
+        if (listElem[i].style.visibility == "visible") {
+            // Delete className added in makeVisible
+            listElem[i].className = listElem[i].className.slice(0, listElem[i].className.length - 15)
+            listElem[i].style.setProperty("position", "absolute", "important");
+            listElem[i].style.visibility = "hidden";
         }
     }
 }
